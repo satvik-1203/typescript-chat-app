eae2203392a615a1a09cc2481115caa611e0007a
satvik-1203 <satvikgsu@gmail.com>
2021-06-28 09:30:56 +0530

    auth middleware

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,1 +1,18 @@
-import express, { Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import express, { Request, Response, NextFunction } from "express";
+require("dotenv").config();
+
+export default (req: Request, res: Response, next: NextFunction) => {
+  const token: string | undefined | string[] = req.headers["JWT-TOKEN"];
+  if (!token) return console.log("no token");
+  if (!process.env["JWT-SIGN"]) return console.log("No sign");
+  try {
+    if (typeof token === "object") return;
+    const payload = jwt.verify(token, process.env["JWT-SIGN"]);
+    if (!payload) res.status(400).send("Invalid Token");
+    req.body["JWT-payload"] = payload;
+    next();
+  } catch {
+    res.status(400).send("Invalid Token");
+  }
+};
