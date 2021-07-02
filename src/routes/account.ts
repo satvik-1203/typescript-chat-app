c9399dde456d66be159de3f82cfb643a7f9b083c
satvik-1203 <satvikgsu@gmail.com>
2021-07-02 08:25:03 +0530

    fixes

--- src/routes/account.ts
+++ src/routes/account.ts
@@ -7,10 +7,9 @@ import IUser from "../interface/IUser";
 
 accountRouter.get("/me", auth, async (req, res) => {
   try {
-    const payload = req.body["jwt-payload"];
-    const user = await UserModel.findOne({ email: payload.email }).exec();
-    if (!user) return res.status(404).send("No user found");
-    res.send({ email: user.email, username: user.username, id: user._id });
+    const { email, id, username } = req.body["jwt-payload"];
+
+    res.send({ email: email, username: username, id: id });
   } catch (err) {
     res.send(err);
   }
