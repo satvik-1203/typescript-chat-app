b0c6d0eeaafcf0000e7ec47b8bb3786a4d19bf8b
shah1012 <69828183+shah1012@users.noreply.github.com>
2021-07-12 20:50:23 -0500

    Added sockets

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,13 @@ console.clear();
 import express from "express";
 import { mainRouter } from "./express";
 import mongoose from "mongoose";
+import { Server } from "socket.io";
+
+const io = new Server(3002, {
+  cors: {
+    origin: ["http://localhost:3000"],
+  },
+});
 
 mongoose
   .connect("mongodb://localhost:27017/sdMain", {
@@ -16,6 +23,16 @@ mongoose
 const app = express();
 
 app.use("/api", mainRouter);
+
+// sockets
+
+io.on("connection", (socket) => {
+  console.log(socket.id);
+
+  socket.on("send message", (message: string) => {
+    socket.broadcast.emit("receive-message", message);
+  });
+});
 
 app.listen(3001, () => {
   console.log("Listening on port 3001...");
